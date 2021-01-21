3041705f425e98baa1a5eadf206dfa7d809279a4
JWarson <73791350+JWarson@users.noreply.github.com>
2021-01-21 18:08:33 +0100

    Add files via upload

--- visualisation.r
+++ visualisation.r
@@ -10,12 +10,27 @@ parasitedata <- read.csv(gsub(" ", "",paste("standardrun_Parasite_",run.date,"_s
 head(parasitedata)
 
 #remove 'NA' genotype
-gt <- subset(hostdata, Immune.Genotype=='1'| Immune.Genotype=='2'| Immune.Genotype=='3'|Immune.Genotype=='4'|Immune.Genotype=='5' )
+gtH <- subset(hostdata, Immune.Genotype=='1'| Immune.Genotype=='2'| Immune.Genotype=='3'|Immune.Genotype=='4'|Immune.Genotype=='5' )
+gtP <- subset(parasitedata, Parasite.Infection.Genotype=='1'| Parasite.Infection.Genotype=='2'| Parasite.Infection.Genotype=='3'|Parasite.Infection.Genotype=='4'|Parasite.Infection.Genotype=='5' )
 
-as.factor(gt$Immune.Genotype)
-plot(Host.Number.Individuals ~ Host.Time, data = gt, col = Immune.Genotype, pch = Host.Population )
-plot(Host.Number.Individuals ~ Host.Time, data = gt, pch = Immune.Genotype, col = Host.Population )
+#host
+ggplot(gtH, aes(Host.Time)) + 
+  geom_point(aes(y = Host.Number.Individuals, colour = as.factor(Immune.Genotype), pch = as.factor(Host.Replicate))) +
+  labs(x="Time (years)", y = "Host Individuals", colour = "Genotype", pch = "Replicate", 
+       title = "Resource 500") +
+  scale_shape_manual(values=c(3, 19))+
+  theme_bw()
 
+#parasite
+gtP2 <- subset(parasitedata, Parasite.Replicate == 2 & (Parasite.Infection.Genotype=='1'| Parasite.Infection.Genotype=='2'| Parasite.Infection.Genotype=='3'|Parasite.Infection.Genotype=='4'|Parasite.Infection.Genotype=='5'))
+ggplot(gtP2, aes(Parasite.Time)) + 
+  geom_point(aes(y = Parasite.Number.Individuals, colour = as.factor(Parasite.Infection.Genotype), pch = as.factor(Parasite.Replicate))) +
+  labs(x="Time (years)", y = "Parasite Individuals", pch = "Replicate", colour = "Genotype", 
+       title = "Resource 500") +
+  scale_shape_manual(values=c(3, 16))+
+  theme_bw()
+
+################################################################################
 ##Stats file###
 ##save and load stats
 write.table(stats, file = result.file.name, sep = "\t")
@@ -29,7 +44,7 @@ ggplot(data, aes(time)) +
   geom_point(aes(y = av.host.popsize, colour = "Gentian")) +
   geom_point(aes(y = av.parasite.popsize, colour = "Alcon")) +
   labs(x="Time (years)", y = "Average population size", colour = "Species", 
-       title = "Resource 500") +
+       title = "Resource 50") +
   theme_bw()
 
 #equilibrium Host/parasite Ratio
@@ -39,12 +54,15 @@ hoverp <- eqh/eqp
 c(eqh, eqp, hoverp)
 
 #equilibrium in function of resource (calculated from above 'hoverp')
-qplot(x = c(250,500,1000,1500,2000), y = c(2.04520547945205,
-                                           2.04794520547945,
-                                           2.05151098901099,
-                                           2.05791176231976,
-                                           2.07074340527578), 
-      geom = "line", xlab = 'Resources', ylab = "Host/Parasite equilibrium") +
+qplot(x = c(20,50,75,100,250,500,1000,2000), y = c(1.852719,
+                                                1.935147,
+                                                1.865336,
+                                                1.870178,
+                                                1.917626,
+                                                1.945995,
+                                                1.942982,
+                                                1.944654), 
+      geom = c("line","point"), xlab = 'Resources', ylab = "Host/Parasite equilibrium") +
   theme_bw()
 
 #equilibrium in function of starting P/H ratio 
